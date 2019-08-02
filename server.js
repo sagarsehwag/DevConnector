698f5808c15d489423d8127fd404774abedd8696
Sagar Sehwag <isagarsehwag@gmail.com>
2019-08-02 13:55:54 +0530

    Prepared for deployment
    Signed-off-by: Sagar Sehwag <isagarsehwag@gmail.com>

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
@@ -15,12 +16,13 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/posts", require("./routes/api/posts"));
 app.use("/api/profile", require("./routes/api/profile"));
 
-app.get("/", (req, res) => {
-	res.json({
-		success: true,
-		message: "Welcome to DevConnector API"
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("*", (req, res) => {
+		res.send(path.resolve(__dirname, "client", "build", "index.html"));
 	});
-});
+}
 
 // Server Initialisation
 const PORT = process.env.PORT || 5000;
