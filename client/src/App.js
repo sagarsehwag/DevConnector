e0975ae537e4fe44c7bc8bd5409bbd49200cc5a3
Sagar Sehwag <isagarsehwag@gmail.com>
2019-08-02 12:55:43 +0530

    NotFoundPage
    Signed-off-by: Sagar Sehwag <isagarsehwag@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,21 +3,9 @@ import { BrowserRouter, Route, Switch } from "react-router-dom";
 import { Provider } from "react-redux";
 
 import Navbar from "./components/layout/Navbar";
-import Alert from "./components/layout/Alert";
 import Landing from "./components/layout/Landing";
-import Login from "./components/auth/Login";
-import Register from "./components/auth/Register";
-import Dashboard from "./components/dashboard/Dashboard";
-import CreateProfile from "./components/profile-forms/CreateProfile";
-import EditProfile from "./components/profile-forms/EditProfile";
-import AddExperience from "./components/profile-forms/AddExperience";
-import AddEducation from "./components/profile-forms/AddEducation";
-import Profiles from "./components/profiles/Profiles";
-import Profile from "./components/profile/Profile";
-import Posts from "./components/posts/Posts";
-import Post from "./components/post/Post";
 
-import PrivateRoute from "./components/routing/PrivateRoute";
+import Routes from "./components/routing/Routes";
 
 // Authorisation
 import setAuthToken from "./utils/setAuthToken";
@@ -38,23 +26,10 @@ const App = () => {
 			<BrowserRouter>
 				<Fragment>
 					<Navbar />
-					<Route exact path="/" component={Landing} />
-					<section className="container p-5">
-						<Alert />
-						<Switch>
-							<Route exact path="/register" component={Register} />
-							<Route exact path="/login" component={Login} />
-							<Route exact path="/profiles" component={Profiles} />
-							<Route exact path="/profile/:id" component={Profile} />
-							<PrivateRoute exact path="/dashboard" component={Dashboard} />
-							<PrivateRoute exact path="/create-profile" component={CreateProfile} />
-							<PrivateRoute exact path="/edit-profile" component={EditProfile} />
-							<PrivateRoute exact path="/add-experience" component={AddExperience} />
-							<PrivateRoute exact path="/add-education" component={AddEducation} />
-							<PrivateRoute exact path="/posts" component={Posts} />
-							<PrivateRoute exact path="/posts/:id" component={Post} />
-						</Switch>
-					</section>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route component={Routes} />
+					</Switch>
 				</Fragment>
 			</BrowserRouter>
 		</Provider>
---
+++ client/src/components/layout/NotFoundPage.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const NotFoundPage = (props) => {
+	return (
+		<div>
+			<h1>
+				<i className="fas fa-exclamation-triangle" /> Page Not Found
+			</h1>
+		</div>
+	);
+};
+
+export default NotFoundPage;
